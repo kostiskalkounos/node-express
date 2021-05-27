c1b8d87601a46ecd7875292fbac11eed96e005fb
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-28 00:56:55 +0200

    get token from the url

--- controllers/auth.js
+++ controllers/auth.js
@@ -171,3 +171,25 @@ exports.postReset = (req, res, next) => {
       });
   });
 };
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token; // gets the token we use in /router/auth
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } }) // $gt = greater than
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
