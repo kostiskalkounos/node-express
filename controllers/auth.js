dc1ad3784a79d95376d61a2b4e0f50bdeda3164e
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-28 00:22:22 +0200

    create a token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const User = require("../models/user");
 
@@ -133,5 +134,40 @@ exports.getReset = (req, res, next) => {
     pageTitle: "Reset Password",
     path: "/reset",
     errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(() => {
+        res.redirect("/");
+        transporter.sendMail({
+          to: req.body.email,
+          from: "shop@node-complete.com",
+          subject: "Password reset",
+          html: `
+          <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
+          `,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -15,4 +15,6 @@ router.post("/signup", authController.postSignup);
 
 router.post("/logout", authController.postLogout);
 
+router.post("/reset", authController.postReset);
+
 module.exports = router;
