61181e58950938b79f6f573c62cac5cae9e073d4
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-27 12:32:37 +0200

    set a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").trim().split("=")[1];
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
+    isAuthenticated: isLoggedIn,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  // res.cookie("Set-Cookie", "loggedIn=true"); this is newer
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
